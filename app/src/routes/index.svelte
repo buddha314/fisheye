d847422560bf82974e8afe2175c005fbc68e071b
buddha314 <buddha_314@protonmail.com>
2022-08-03 19:59:37 +0000

    Commit made from VS Code Azure Static Web Apps

--- app/src/routes/index.svelte
+++ app/src/routes/index.svelte
@@ -1,2 +1,3 @@
-<h1>Welcome to SvelteKit</h1>
+<h1>Welcome to Fisheye</h1>
+<h2>Well, the Svelte App bit, anyway</h2>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
